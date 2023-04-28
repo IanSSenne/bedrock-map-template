1ba2f0e457b616089b5470af4687c3f93f4a706d
Ian Senne <ian@iansenne.com>
2023-04-28 02:32:48 -0700

    clean up un needed files

--- lib/mcf/lang.ts
+++
@@ -1,6 +0,0 @@
-import { getResource } from "../resource";
-import { ResourceWithExt } from "../types";
-
-export function compile(resource: ResourceWithExt<"glsl">) {
-  console.log(getResource(resource));
-}
